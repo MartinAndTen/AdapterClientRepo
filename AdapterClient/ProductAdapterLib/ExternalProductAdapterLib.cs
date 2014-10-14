bdcbe1ba9c9e7b6862cd62dc7b18af950ebb9d24
Martin Andersson-Tenninge <martin.andersson-tenninge@yh.nackademin.se>
2014-10-14 10:11:35 +0200

    added first support for adapter class lib

---
+++ AdapterClient/ProductAdapterLib/ExternalProductAdapterLib.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductAdapterLib
+{
+    public class ExternalProductAdapterLib
+    {
+        decimal _price;
+        string _productName;
+        string _code;
+    }
+}
