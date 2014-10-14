c4ae9d721ef05ea80df4b4a776571e6e9d76fa16
Martin Andersson-Tenninge <martin.andersson-tenninge@yh.nackademin.se>
2014-10-14 09:47:53 +0200

    Renamed and added code for unit test + class lib project

---
+++ AdapterClient/AdapterTest/ProductTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProductLib;
+
+namespace AdapterTest
+{
+    [TestClass]
+    public class ProductTest
+    {
+        [TestMethod]
+        public void TestIfProductReturnsCorrectValue()
+        {
+            //Arrange
+            string expectedCode = "1234";
+            decimal expectedPrice = 99;
+            string expectedProductName = "honda";
+            //Act
+            var product = new Product("honda", 99, "1234");
+            //Assert
+            Assert.AreEqual(expectedCode, product.Code);
+            Assert.AreEqual(expectedPrice, product.Price);
+            Assert.AreEqual(expectedProductName, product.ProductName);
+        }
+    }
+}
